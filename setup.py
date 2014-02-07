dde4eb494362b7a5d37cca14fccd2b11737e00a5
Justin Findlay <jfindlay@gmail.com>
2014-02-07 13:20:51 -0700

    adding setup.py
    pscreen can now be installed in a standard way

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pscreen',
+    # TODO: get all this info from git automatically: 'import git'
+    version='1.0',
+    description='Python Distribution Utilities',
+    author='Justin Findlay',
+    author_email='jfindlay@gmail.com',
+    url='http://github.com/jfindlay/pscreen/',
+    package=['utils']
+    )
