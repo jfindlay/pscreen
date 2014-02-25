ce1b53476b7492ba6f7bbe9774858cd6e33692b5
Justin Findlay <jfindlay@gmail.com>
2014-02-24 22:11:53 -0700

    made some source changes; install script is broken
    http://stackoverflow.com/questions/8405271/can-distutils-perform-a-dependency-check-without-installing/8419012#8419012

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from subprocess import Popen,PIPE
 
-setup(name='pscreen',
-    # TODO: get all this info from git automatically: 'import git'
-    version='1.0',
+def test_screen():
+  for path in re.split(':',os.environ['PATH']):
+    if os.path.exists(os.path.join(path,'screen')):
+      return
+  sys.stderr.write('GNU screen not found.\n') ; sys.stderr.flush()
+  sys.exit(1)
+
+def get_version():
+  return Popen(('git','describe'),stdout=PIPE).stdout.read()
+
+setup(
+    name='pscreen',
+    version=get_version(),
     description='Python Distribution Utilities',
     author='Justin Findlay',
     author_email='jfindlay@gmail.com',
     url='http://github.com/jfindlay/pscreen/',
-    package=['utils']
     )
